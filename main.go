4cc9cb3b0d6f68b7f8dec4ca0701018c35fb1498
Andre Diniz <andre_fcd@yahoo.com.br>
2024-12-01 16:45:14 +0000

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/AndreCDiniz/ApiRestFull-Go/router"
+import (
+	"github.com/AndreCDiniz/ApiRestFull-Go/config"
+	"github.com/AndreCDiniz/ApiRestFull-Go/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize Configs
+	error := config.Init()
+	if error != nil {
+		logger.Errorf("config initialization error: %v", error)
+		return
+	}
+
 	// Initialize Router
 	router.Initialize()
 
