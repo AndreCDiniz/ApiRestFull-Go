75b548afd600928dd6ba8df4200628d4f948ea42
Andre Diniz <andre_fcd@yahoo.com.br>
2024-12-01 22:57:27 +0000

    feat: add createOpening handler logic

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -1,12 +1,36 @@
 package handler
 
 import (
+	"github.com/AndreCDiniz/ApiRestFull-Go/schemas"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateOpeningHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"message": "GET Opening",
-	})
+	request := CreateOpeningRequest{}
+
+	context.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(context, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("error creating opening: %v", err.Error())
+		sendError(context, http.StatusInternalServerError, "error creating opening on database")
+		return
+	}
+
+	sendSuccess(context, "create-opening", opening)
 }
--- schemas/opening.go
+++ schemas/opening.go
@@ -1,13 +1,29 @@
 package schemas
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Opening struct {
 	gorm.Model
-	Role     string `json:"role"`
-	Company  string `json:"company"`
-	Location string `json:"location"`
-	Remote   bool   `json:"remote"`
-	Link     string `json:"link"`
-	Salary   int64  `json:"salary"`
+	Role     string
+	Company  string
+	Location string
+	Remote   bool
+	Link     string
+	Salary   int64
+}
+
+type OpeningResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdateAt  time.Time `json:"updateAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
 }
